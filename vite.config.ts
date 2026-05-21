6afc9270a0251288753d63aefb5753aae711978e
unknown <you>
2026-05-21 02:07:11 +0200

    change

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  base: '/opgave-typescript/',   // ← This line must be here
 });
-
-base: '/opgave-typescript/'
